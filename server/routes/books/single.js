99b892946e5b757c901c798cde3ff9801dfddf3f
EQuimper <quimperemanuel@gmail.com>
2016-05-14 17:05:20 -0600

    Change some stuff on routes

--- server/routes/books/single.js
+++ server/routes/books/single.js
@@ -1,11 +1,10 @@
 const Book = require('../../db/models/books');
 
 module.exports = (req, res) => {
-	Book.findById({_id: req.params.id}, (err, book) => {
-		if(err)
-			res.status(404).json({
-				message: 'Book not found!'
-			});
-		res.status(200).json({book});
-	});
+    Book.findById({_id: req.params.id}, (err, book) => {
+        if(err)
+            res.status(404);
+        else
+            res.json(book).status(200);
+    });
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 const routes = require('express').Router();
 const books = require('./books/index');
 
-routes.get('/', (req, res) => {
-	res
-		.status(200)
-		.json({
-			message: 'Welcome to the api'
-		});
+routes.get('/api', (req, res) => {
+    res
+        .status(200)
+        .json({
+            message: 'Welcome to the api'
+        });
 });
 
-routes.use('/books', books);
+routes.use('/api/books', books);
 
 module.exports = routes;
